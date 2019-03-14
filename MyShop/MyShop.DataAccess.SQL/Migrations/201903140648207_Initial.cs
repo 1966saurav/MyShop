816537b17265b68eda06a28d1e139b719d824ca0
1966saurav <ssharma88823@gmail.com>
2019-03-15 00:07:15 +0530

    Dependency Injection Unity

---
+++ MyShop/MyShop.DataAccess.SQL/Migrations/201903140648207_Initial.cs
@@ -0,0 +1,42 @@
+namespace MyShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ProductCategories",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        category = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        Id = c.String(nullable: false, maxLength: 128),
+                        category = c.String(maxLength: 20),
+                        Name = c.String(),
+                        Description = c.String(),
+                        Price = c.String(),
+                        Image = c.String(),
+                        CreatedAt = c.DateTimeOffset(nullable: false, precision: 7),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+            DropTable("dbo.ProductCategories");
+        }
+    }
+}
